09316681863342bc8d2735ec33ebcb25aa8ca424
xKommando <xcommando@outlook.com>
2014-04-03 17:00:06 +0800

    test comper repo in team blacklancer

--- src/org/wangk/comper/model/WKTestQuestion.java
+++ src/org/wangk/comper/model/WKTestQuestion.java
@@ -8,6 +8,23 @@ import org.wangk.comper.db.orm.RowMapping;
 
 public class WKTestQuestion implements Serializable {
 
+	public static class Type {
+		public static final int MULTI_CHOICE 	= 0;
+		public static final int FILL_BLANKS 	= 1;
+		public static final int SIMPLE_QA 		= 2;
+		public static final int ADDITIONAL 		= 3;
+	}
+	
+	public int				id;
+	public int				type;
+	public int				credit;
+	public float			difficulty;
+	public String			description;
+	public int				id_paper;
+	
+	
+	
+	
 	private static final long serialVersionUID = -6048408374797399234L;
 	
 	public static final RowMapping<WKTestQuestion> MAPPING = new RowMapping<WKTestQuestion>() {
